4963f80f848fc55230a44fd424b6a56afc407d09
Elena7019 <108617633+Elena7019@users.noreply.github.com>
2023-02-09 18:37:55 +0300

    roman conversion method

---
+++ Task4.java
@@ -0,0 +1,44 @@
+// 4. Написать метод, который переведёт данное целое число в римский формат.
+
+package DomashkiJava.Hw5Java;
+import java.util.TreeMap;
+
+public class Task4 {
+    public static void main(String[] args) { }
+        private final static TreeMap<Integer, String> map = new TreeMap<Integer, String>();
+        private static final Task4 RomanNumber = null;
+
+    static {
+
+        map.put(1000, "M");
+        map.put(900, "CM");
+        map.put(500, "D");
+        map.put(400, "CD");
+        map.put(100, "C");
+        map.put(90, "XC");
+        map.put(50, "L");
+        map.put(40, "XL");
+        map.put(10, "X");
+        map.put(9, "IX");
+        map.put(5, "V");
+        map.put(4, "IV");
+        map.put(1, "I");
+
+    }
+
+    public final String toRoman(int number) {
+        int l =  map.floorKey(number);
+        if ( number == l ) {
+            return map.get(number);
+        }
+        return map.get(l) + toRoman(number-l);
+    }
+
+    public void testRomanConversion() {
+
+    for (int i = 1; i<= 100; i++) {
+        System.out.println(i+"\t =\t "+RomanNumber.toRoman(i));
+    }
+
+}
+}
